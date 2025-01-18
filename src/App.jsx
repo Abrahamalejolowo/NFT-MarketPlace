b45c132ab3396a9a4dbc311d531a956d64eb7ad5
Alejolowoabraham <abrahamalejolowo@gmail.com>
2025-01-18 04:47:30 -0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
 import "./App.css";
-import BrowseCategory from "./component/BrowseCategory";
-import DiscoverMore from "./component/DiscoverMore";
 import FooterSce from "./component/FooterSec";
 import Header from "./component/Header";
-import HeroSec from "./component/HeroSec";
-import HowITWorks from "./component/HowITWorks";
-import NftHighlights from "./component/NftHighlights";
-import TrendingCol from "./component/TrendingCol";
-import TopCreators from "./component/TopCreators";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./page/HomePage";
 import Marketplace from "./page/Marketplace";
@@ -16,7 +9,7 @@ import Connect from "./page/Connect";
 import ScrollToTop from "./component/ScrollToTop";
 import Signup from "./page/Signup";
 import About from "./component/About";
-import Login from "./component/Login";
+import Login from "./page/Login";
 
 function App() {
   return (
@@ -31,7 +24,7 @@ function App() {
           <Route path="/marketplace/:id" element={<About />} />
           <Route path="/rankings" element={<Rankings />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/Login" element={<Login />} />
         </Routes>
         <FooterSce />
       </div>
--- src/component/marketplace.jsx
+++ src/component/marketplace.jsx
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-unused-vars
 import React from "react";
 import { NFTS } from "./Database";
 import { Link } from "react-router-dom";
